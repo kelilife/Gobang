30c54f5014ade91fba3ab37f5fe05568c398a0ac
KeLi <kelilive@outlook.com>
2022-10-14 01:26:30 +0800

    Update

--- KeLi.Gobang/Properties/AssemblyInfo.cs
+++ KeLi.Gobang/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KeLi.Gobang")]
+[assembly: AssemblyTitle("Gobang")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KeLi.Gobang")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCompany("KeLi")]
+[assembly: AssemblyProduct("Gobang")]
+[assembly: AssemblyCopyright("Copyright © KeLi 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
